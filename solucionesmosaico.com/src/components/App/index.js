836e0c15b7524e740b67e52687fde3ed0bc05f8a
memowii <memogl25@gmail.com>
2020-08-14 19:52:13 -0500

    Delefe unsed files; Restructure project files

---
+++ solucionesmosaico.com/src/components/App/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+export function App() {
+  return (
+    <div className="App">
+      <div class="jumbotron">
+        <h1 class="display-4">App</h1>
+        <p class="lead">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+      </div>
+    </div>
+  );
+}
