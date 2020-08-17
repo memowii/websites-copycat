d64f9ff615811fd335ed6c49760b5754595aaee3
memowii <memogl25@gmail.com>
2020-08-17 14:48:59 -0500

    Change component Navbar

--- solucionesmosaico.com/src/components/Navbar/index.js
+++ solucionesmosaico.com/src/components/Navbar/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar as RsNavbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+  // NavbarText,
+} from "reactstrap";
 
 export function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <div>navbar</div>
-  )
+    <RsNavbar expand="md" light color="white" >
+      <NavbarToggler onClick={toggle} className="ml-auto" />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink href="#">HOME</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">EQUIPO</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">BLOG</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">#JuntosPorLasMiPyMEs</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </RsNavbar>
+  );
 }
--- solucionesmosaico.com/src/index.js
+++ solucionesmosaico.com/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import { App } from "./components/App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById("root")
 );
