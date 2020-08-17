9a79dd1743c7b982d869113540506866cf047a8e
memowii <memogl25@gmail.com>
2020-08-17 17:48:25 -0500

    Retouch Navbar component

--- solucionesmosaico.com/src/components/Navbar/index.js
+++ solucionesmosaico.com/src/components/Navbar/index.js
@@ -8,27 +8,40 @@ import {
   NavLink,
 } from "reactstrap";
 
+import "./index.css";
+
 export function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <RsNavbar expand="md" light color="white" >
-      <NavbarToggler onClick={toggle} className="ml-auto" />
+    <RsNavbar expand="md" light color="white" className="p-0">
+      <div className="px-3 py-2 w-100 d-flex justify-content-end">
+        <NavbarToggler onClick={toggle} className="" />
+      </div>
+
       <Collapse isOpen={isOpen} navbar>
-        <Nav className="mr-auto" navbar>
+        <Nav className="nav--gray nav--font-open-sans" navbar>
           <NavItem>
-            <NavLink href="#">HOME</NavLink>
+            <NavLink href="#" className="nav-link--hover-blue px-4 border-bottom border-top">
+              HOME
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="#">EQUIPO</NavLink>
+            <NavLink href="#" className="nav-link--hover-blue px-4 border-bottom">
+              EQUIPO
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="#">BLOG</NavLink>
+            <NavLink href="#" className="nav-link--hover-blue px-4 border-bottom">
+              BLOG
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="#">#JuntosPorLasMiPyMEs</NavLink>
+            <NavLink href="#" className="nav-link--hover-blue px-4 border-bottom">
+              #JuntosPorLasMiPyMEs
+            </NavLink>
           </NavItem>
         </Nav>
       </Collapse>
