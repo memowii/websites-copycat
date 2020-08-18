23ec158ffb138fd9676eedbd4263c89d13651b91
memowii <memogl25@gmail.com>
2020-08-18 17:24:04 -0500

    Fix styles for navbar component

--- solucionesmosaico.com/src/components/Navbar/index.js
+++ solucionesmosaico.com/src/components/Navbar/index.js
@@ -17,29 +17,29 @@ export function Navbar() {
 
   return (
     <RsNavbar expand="lg" light color="white" className="p-0">
-      <div className="px-3 py-2 w-100 d-flex justify-content-end">
+      <div className="px-3 py-2 w-100 d-flex justify-content-end d-lg-none">
         <NavbarToggler onClick={toggle} className="" />
       </div>
 
       <Collapse isOpen={isOpen} navbar>
-        <Nav className="nav--gray nav--font-open-sans" navbar>
+        <Nav className="nav--gray nav--font-open-sans navbar-nav--expanded" navbar>
           <NavItem>
-            <NavLink href="#" className="nav-link--hover-blue px-4 border-bottom border-top">
+            <NavLink href="#" className="nav-link--hover-blue nav-link--border px-4">
               HOME
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="#" className="nav-link--hover-blue px-4 border-bottom">
+            <NavLink href="#" className="nav-link--hover-blue nav-link--border px-4">
               EQUIPO
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="#" className="nav-link--hover-blue px-4 border-bottom">
+            <NavLink href="#" className="nav-link--hover-blue nav-link--border px-4">
               BLOG
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="#" className="nav-link--hover-blue px-4 border-bottom">
+            <NavLink href="#" className="nav-link--hover-blue nav-link--border px-4">
               #JuntosPorLasMiPyMEs
             </NavLink>
           </NavItem>
