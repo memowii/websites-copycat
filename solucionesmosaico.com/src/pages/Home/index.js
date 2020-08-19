baa8984c699984d0b33b0fa5c723a3d6f7492a5e
memowii <memogl25@gmail.com>
2020-08-19 17:33:35 -0500

    Add element card

--- solucionesmosaico.com/src/pages/Home/index.js
+++ solucionesmosaico.com/src/pages/Home/index.js
@@ -1,14 +1,35 @@
 import React from "react";
-import { Divider } from '../../components/Divider'
+import { Divider } from "../../components/Divider";
+
+import estrategiaImg from "../../images/estrategia.png";
 
 export function Home() {
   return (
-    <div>
-      <Divider/>
+    <div className="container">
+      <Divider />
 
       <h1 className="text-center h4 font-weight-bolder">
         Estrategiasa y soluciones creativas
       </h1>
+
+      <h2 className="h5 font-weight-bolder text-center mt-4">
+        Nuestros servicios
+      </h2>
+
+      <div className="card" style={{ backgroundColor: "#9faee5" }}>
+        <div className="card-body">
+          <div className="text-center">
+            <img src={estrategiaImg} alt="" />
+          </div>
+
+          <h5 className="card-title">Estrategia</h5>
+          <ul>
+            <li>Campaña publicitaria</li>
+            <li>Gestión de proyectos ante instituciones públicas y privadas</li>
+            <li>Manual organizacional</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
