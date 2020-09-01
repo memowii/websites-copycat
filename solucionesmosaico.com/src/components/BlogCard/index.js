e547bbdd62dea7fe39241c60bc1c34ff9ac346c8
memowii <memogl25@gmail.com>
2020-09-01 18:02:21 -0500

    Add Link for component BlogCard

--- solucionesmosaico.com/src/components/BlogCard/index.js
+++ solucionesmosaico.com/src/components/BlogCard/index.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 
 export const BlogCard = ({ img, text, link }) => (
   <div className="card text-center">
-    <img src={img} className="card-img-top" alt="Estrategia de negocios" />
+    <Link to={link}>
+      <img src={img} className="card-img-top" alt="Estrategia de negocios" />
+    </Link>
     <div className="card-body">
       <p className="card-text">{text}</p>
       <Link to={link} className="btn btn-outline-dark">
