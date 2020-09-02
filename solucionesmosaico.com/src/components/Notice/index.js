ebc24eca48729386dc6faa0297de37e3f33ef7ea
memowii <memogl25@gmail.com>
2020-09-02 17:29:16 -0500

    Add component Notice

---
+++ solucionesmosaico.com/src/components/Notice/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./index.css";
+
+export const Notice = ({ children }) => {
+  return <div className="Notice px-5 py-3">{children}</div>;
+};
--- solucionesmosaico.com/src/pages/JuntosPymes/index.js
+++ solucionesmosaico.com/src/pages/JuntosPymes/index.js
@@ -2,8 +2,7 @@ import React, { useEffect } from "react";
 
 import { InnerHeading } from "../../components/InnerHeading";
 import { Divider } from "../../components/Divider";
-
-import "./index.css";
+import { Notice } from "../../components/Notice";
 
 export const JuntosPymes = () => {
   useEffect(() => {
@@ -17,42 +16,45 @@ export const JuntosPymes = () => {
   });
 
   return (
-    <div className="JuntosPymes container py-4">
-      <InnerHeading
-        title="Juntos por las MiPyMes"
-        subtitle="¡Gracias a quienes participaron!"
-      />
+    <div className="container">
+      <Notice>
+        <InnerHeading
+          title="Juntos por las MiPyMes"
+          subtitle="¡Gracias a quienes participaron!"
+        />
 
-      <Divider mid className="mb-4" />
+        <Divider mid className="mb-4" />
 
-      <p>
-        La situación económica derivada por la <strong>COVID-19</strong> ha
-        afectado a las <strong>micro</strong>,<strong>pequeñas</strong> y{" "}
-        <strong>medianas empresas</strong>, quienes responsablemente decidieron
-        cerrar de forma temporal sus establecimientos, debiendo trasladarse al
-        ámbito digital.
-      </p>
+        <p>
+          La situación económica derivada por la <strong>COVID-19</strong> ha
+          afectado a las <strong>micro</strong>,<strong>pequeñas</strong> y{" "}
+          <strong>medianas empresas</strong>, quienes responsablemente
+          decidieron cerrar de forma temporal sus establecimientos, debiendo
+          trasladarse al ámbito digital.
+        </p>
 
-      <p>
-        Conscientes de ello y de lo complejo que puede resultar dicho proceso,
-        lanzamos la{" "}
-        <a href="#">
-          convocatoria <strong>#JuntosPorLasMiPyMes</strong>
-        </a>
-        .
-      </p>
+        <p>
+          Conscientes de ello y de lo complejo que puede resultar dicho proceso,
+          lanzamos la{" "}
+          <a href="#">
+            convocatoria <strong>#JuntosPorLasMiPyMes</strong>
+          </a>
+          .
+        </p>
 
-      <p>
-        Elegiremos a <strong>tres negocios</strong> para brindarles una{" "}
-        <strong>auditoría de marketing digital sin costo</strong>. Te invitamos
-        a seguirnos en <a href="#">Facebook</a> o <a href="#">Instagram</a>,
-        donde daremos a conocer los resultados el 27 de junio, Día de las
-        Microempresas y las Pequeñas y Medianas Empresas.
-      </p>
+        <p>
+          Elegiremos a <strong>tres negocios</strong> para brindarles una{" "}
+          <strong>auditoría de marketing digital sin costo</strong>. Te
+          invitamos a seguirnos en <a href="#">Facebook</a> o{" "}
+          <a href="#">Instagram</a>, donde daremos a conocer los resultados el
+          27 de junio, Día de las Microempresas y las Pequeñas y Medianas
+          Empresas.
+        </p>
 
-      <p>
-        <a href="#">Términos y condiciones</a>
-      </p>
+        <p>
+          <a href="#">Términos y condiciones</a>
+        </p>
+      </Notice>
     </div>
   );
 };
