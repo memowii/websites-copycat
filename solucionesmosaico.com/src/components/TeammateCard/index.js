6d5bac2cf970cd23d5886f7379fac1e6d6c008f2
memowii <memogl25@gmail.com>
2020-09-04 17:20:27 -0500

    Fix styles of component TeammateCard

--- solucionesmosaico.com/src/components/TeammateCard/index.js
+++ solucionesmosaico.com/src/components/TeammateCard/index.js
@@ -5,12 +5,12 @@ import "./index.css";
 export const TeammateCard = ({ name, job, bio, bgc }) => {
   return (
     <div
-      className="TeammateCard d-flex flex-column text-center"
+      className="TeammateCard d-flex flex-column text-center text-md-left"
       style={{ backgroundColor: bgc }}
     >
       <h3 className="TeammateCard_name font-weight-bolder mb-3">{name}</h3>
       <p className="TeammateCard__job text-uppercase">{job}</p>
-      <p className="TeammateCard__bio">{bio}</p>
+      <p className="TeammateCard__bio text-justify">{bio}</p>
     </div>
   );
 };
